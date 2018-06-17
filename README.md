a558ddb8196340dd3e23b6ef2c87e903acfcfaf5
John Dawes <jdawes@bluebitmedia.com>
2018-06-17 15:32:32 -0600

    User menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python Blockchain Experiment
+
+
--- blockchain.py
+++ blockchain.py
@@ -22,18 +22,40 @@ def get_user_input():
     """ Returns the input of the user transaction amount as a float. """
     return float(input('Your transaction amount: '))
 
+def get_user_choice():
+    user_input = input('Enter your choice: ')
+    return user_input
+
+def print_blockchain():
+    print('Outputting block')
+    print(block)
+    print('Blockchain output completed.')
+
 # The Gensis Block
 tx_amount = get_user_input()
 add_value(tx_amount)
 
 while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
+    print('Please choose a menu f(x):')
+    print('(1) Add a new transaction to the blockchain')
+    print('(2) Print the blockchain')
+    print('(q) Quit')
+    user_choice = get_user_choice()
 
-    # Output blockchain to the console
-    for block in blockchain:
-        print('Outputting block')
-        print(block)
+    if user_choice == '1':
+        tx_amount = get_user_input()
+        add_value(tx_amount, get_last_blockchain_value())
+        print('Transaction Added.')
+    elif user_choice == '2':
+        # Output blockchain to the console
+        for block in blockchain:
+            print_blockchain()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid, please select a value from the list!')
+
 # end while True
 
+# Program complete
 print('Done!')
